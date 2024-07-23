6e846db568004f285a9ae60fab3b8ef6d135b3dd
briskaputraa <briskaananda103@gmail.com>
2024-07-23 23:27:45 +0700

    Focus Update

--- src/components/forms/Overviewform/index.tsx
+++ src/components/forms/Overviewform/index.tsx
@@ -1,18 +1,31 @@
 "use client"
 
 import TitleForm from "@/components/atoms/TitleForm"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import CostumUpload from "@/components/ui/organisms/CostumUpload/index"
 import FieldInput from "@/components/ui/organisms/FieldInputs"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger
+} from "@/components/ui/popover"
+  
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Separator } from "@/components/ui/separator"
-import { LOCATION_OPTIONS, optionType } from "@/constants"
+import { EMPLOYEE_OPTIONS, LOCATION_OPTIONS, optionType } from "@/constants"
 import { overviewFormSchema } from "@/lib/form-schema"
+import { cn } from "@/lib/utils"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+
+
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
+  
 
 
 const OverviewForm = () => {
@@ -62,7 +75,7 @@ const OverviewForm = () => {
                     <FormItem>
                         <FormLabel>Website</FormLabel>
                         <FormControl>
-                            <Input className="w-[450px]" placeholder="e.g Twitter" {...field} />
+                            <Input className="w-[450px]" placeholder="https://yourwebsite.com" {...field} />
                         </FormControl>
                         <FormMessage />
                     </FormItem>
@@ -93,12 +106,113 @@ const OverviewForm = () => {
                     </Select>
                     <FormMessage />
                     </FormItem>
+                    )}
+                />
+                
+                <div className="w-[450px] grid grid-cols-2 gap-4">
+                
+                <FormField
+                control={form.control}
+                name="employee"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Employee</FormLabel>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Employee" />
+                            </SelectTrigger>
+                        </FormControl>
+
+                        <SelectContent>
+                            {EMPLOYEE_OPTIONS.map((item: optionType, i:number) => (
+                                <SelectItem key={item.id + i} value={item.id}>
+                                    {item.label}
+                                </SelectItem>
+                            ))}
+                            
+                        </SelectContent>
+                    </Select>
+                    <FormMessage />
+                    </FormItem>
+                    )}
+                />
+
+                <FormField
+                control={form.control}
+                name="industry"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Industry</FormLabel>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Industry" />
+                            </SelectTrigger>
+                        </FormControl>
+
+                        <SelectContent>
+                            {LOCATION_OPTIONS.map((item: optionType, i:number) => (
+                                <SelectItem key={item.id + i} value={item.id}>
+                                    {item.label}
+                                </SelectItem>
+                            ))}
+                            
+                        </SelectContent>
+                    </Select>
+                    <FormMessage />
+                    </FormItem>
+                    )}
+                />
+                    
+
+                </div>
+
+                
+                <FormField
+          control={form.control}
+          name="dateFounded"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date Founded</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    // initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
           )}
         />
+
                 </div>
                 </FieldInput>
-
-
             </form>
         </Form>
     </div>
